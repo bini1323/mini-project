c68089d1913405e7ef19cbf9a2b6b9c850c4214c
TaeHyeon <77439799+xogus3492@users.noreply.github.com>
2024-04-14 18:39:52 +0900

    Create CommonValue.java

---
+++ miniproject/ui/common/CommonValue.java
@@ -0,0 +1,8 @@
+package ui.common;
+
+public abstract class CommonValue {
+	public static final String TITLE = "항공기 부품 관리 시스템";
+	public static final int WIDTH = 600;
+	public static final int HEIGHT = 400;
+	public static final int APPLY_HISTORY_BACKGROUND_COLOR = 0xA9A9A9;
+}
